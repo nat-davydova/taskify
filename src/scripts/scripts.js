e0d50b45a3562bec4c30141ff2e28f81fcbe428c
nat-davydova <natti.davydova@gmail.com>
2020-06-10 17:16:24 +0300

    Search: controller - items became visible after search query changed

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -19,7 +19,8 @@ import {
   markAsError,
   unmarkErrored,
   dismissModal,
-  hideElem
+  hideElem,
+  showElem
 } from './utils'
 
 (function () {
@@ -51,6 +52,9 @@ import {
     state.search.results = search(state.taskList, state.search.query)
 
     const tasksElems = document.querySelectorAll(PATH.task.task)
+
+    tasksElems.forEach(elem => showElem(elem))
+
     tasksElems.forEach(elem => {
       const taskTitle = elem.querySelector(PATH.task.taskTitle).textContent
 
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -52,3 +52,7 @@ export const dismissModal = modal => {
 export const hideElem = elem => {
   elem.classList.add('js-hidden')
 }
+
+export const showElem = elem => {
+  elem.classList.remove('js-hidden')
+}
