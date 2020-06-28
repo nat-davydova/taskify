207f3024cb6d8d92369afbf7a3a3e9ee60dcb1a4
nat-davydova <natti.davydova@gmail.com>
2020-06-28 11:43:08 +0300

    v2.1: controllers refactoring - task controller improved

--- src/scripts/controllers/tasksListController.js
+++ src/scripts/controllers/tasksListController.js
@@ -1,6 +1,6 @@
 import state from '../state'
 
-import { createTask, delTask, editTask } from '../models/taskModel'
+import { createTask, delTask, editTask as edit } from '../models/taskModel'
 import * as taskView from '../views/taskView'
 
 import {
@@ -14,6 +14,52 @@ import {
 
 import { PATH } from '../configs/path'
 
+// *** HELPERS
+const pickItem = taskId => {
+  state.pickedItemId = taskId
+}
+
+const addTask = taskTitle => {
+  if (checkTextEmpty(taskTitle)) {
+    markAsError(PATH.addTask.addTaskTitleInput)
+    return
+  }
+
+  dismissModal(PATH.addTask.addTaskModal)
+
+  const newTask = createTask(taskTitle)
+
+  state.taskList.push(newTask)
+
+  taskView.renderTask(newTask)
+
+  unmarkErrored(PATH.addTask.addTaskTitleInput)
+  cleanInput(PATH.addTask.addTaskTitleInput)
+}
+
+const deleteTask = () => {
+  delTask(state.pickedItemId, state.taskList)
+  dismissModal(PATH.delTaskModal.delModal)
+  taskView.deleteTask(state.pickedItemId)
+}
+
+const editTask = newTaskTitle => {
+  if (checkTextEmpty(newTaskTitle)) {
+    markAsError(PATH.editTaskModal.editTaskInput)
+    return
+  }
+
+  dismissModal(PATH.editTaskModal.editModal)
+
+  edit(state.pickedItemId, state.taskList, newTaskTitle)
+
+  taskView.editTask(state.pickedItemId, newTaskTitle)
+
+  unmarkErrored(PATH.editTaskModal.editTaskInput)
+  cleanInput(PATH.editTaskModal.editTaskInput)
+}
+
+// *** CONTROLLER
 // type can be:
 // - add - for task adding
 // - pick - for picking a task
@@ -22,48 +68,20 @@ export const tasksListController = (type, taskId) => {
   if (type === 'add') {
     const taskTitle = getInputValue(PATH.addTask.addTaskTitleInput)
 
-    if (checkTextEmpty(taskTitle)) {
-      markAsError(PATH.addTask.addTaskTitleInput)
-      return
-    }
-
-    dismissModal(PATH.addTask.addTaskModal)
-
-    const newTask = createTask(taskTitle)
-
-    state.taskList.push(newTask)
-
-    taskView.renderTask(newTask)
-
-    unmarkErrored(PATH.addTask.addTaskTitleInput)
-    cleanInput(PATH.addTask.addTaskTitleInput)
+    addTask(taskTitle)
   }
 
   if (type === 'pick') {
-    state.pickedItemId = taskId
+    pickItem(taskId)
   }
 
   if (type === 'del') {
-    delTask(state.pickedItemId, state.taskList)
-    dismissModal(PATH.delTaskModal.delModal)
-    taskView.deleteTask(state.pickedItemId)
+    deleteTask()
   }
 
   if (type === 'edit') {
     const newTaskTitle = getInputValue(PATH.editTaskModal.editTaskInput)
 
-    if (checkTextEmpty(newTaskTitle)) {
-      markAsError(PATH.editTaskModal.editTaskInput)
-      return
-    }
-
-    dismissModal(PATH.editTaskModal.editModal)
-
-    editTask(state.pickedItemId, state.taskList, newTaskTitle)
-
-    taskView.editTask(state.pickedItemId, newTaskTitle)
-
-    unmarkErrored(PATH.editTaskModal.editTaskInput)
-    cleanInput(PATH.editTaskModal.editTaskInput)
+    editTask(newTaskTitle)
   }
 }
