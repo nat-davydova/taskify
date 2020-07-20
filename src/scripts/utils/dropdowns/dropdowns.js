644865869ef96e4ae80313fc7c81b2b93161e76f
nat-davydova <natti.davydova@gmail.com>
2020-07-20 17:23:13 +0300

    v3.0: dropdowns - toggle dropdown btn

--- src/scripts/utils/dropdowns/dropdowns.js
+++ src/scripts/utils/dropdowns/dropdowns.js
@@ -2,7 +2,13 @@ import { toggleBootstrapBtn, checkElemArgValid } from '../utils'
 
 import { PATH } from '../../configs/path'
 
+const toggleDropdownBtn = btn => {
+  toggleBootstrapBtn(btn)
+}
+
 export const dropdownToggle = dropdown => {
   checkElemArgValid(dropdown, 'dropdownToggle')
-  console.log(dropdown)
+
+  const dropdownBtn = dropdown.querySelector(PATH.dropdown.toggleBtn)
+  toggleDropdownBtn(dropdownBtn)
 }
