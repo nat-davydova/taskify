18788d4a8f4e8fa96b17aeddd29277578429c841
nat-davydova <natti.davydova@gmail.com>
2020-07-24 17:21:58 +0300

    v3.1: filters to MVC - changing state filter value to a current one

--- src/scripts/controllers/filtersController.js
+++ src/scripts/controllers/filtersController.js
@@ -1,9 +1,15 @@
 import state from '../state'
 
 import { filters } from '../models/filtersModel'
+import { stringToCamelCase } from '../utils/utils'
 
 // *** CONTROLLER
 
 export const filtersController = (listToFilter, filterCriteria, currentValue) => {
+
+  const filterCriteriaCamelCase = stringToCamelCase(filterCriteria, '-')
+
+  state.filters[filterCriteriaCamelCase].value = currentValue
+
   console.log(state)
 }
