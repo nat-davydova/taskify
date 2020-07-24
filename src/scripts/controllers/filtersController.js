3d0ffa02f90aa1c52b5f5271aca9423408bc3479
nat-davydova <natti.davydova@gmail.com>
2020-07-24 17:13:34 +0300

    v3.1: filters to MVC - adding model file, basic function and connecting to controller

--- src/scripts/controllers/filtersController.js
+++ src/scripts/controllers/filtersController.js
@@ -1,5 +1,7 @@
 import state from '../state'
 
+import { filters } from '../models/filtersModel'
+
 // *** CONTROLLER
 
 export const filtersController = (listToFilter, filterCriteria, currentValue) => {
