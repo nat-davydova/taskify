89b29f2858e840fbcc576d289dc573902892c48b
nat-davydova <natti.davydova@gmail.com>
2020-12-19 15:56:26 +0300

    port to ts: add airbnb linter

--- src/scripts/utils/utils.ts
+++ src/scripts/utils/utils.ts
@@ -1,4 +1,4 @@
-import PerfectScrollbar from 'perfect-scrollbar'
+import PerfectScrollbar from "perfect-scrollbar";
 
 // COLLECTION OF HELPERS FUNCTIONS
 
@@ -14,146 +14,148 @@ import PerfectScrollbar from 'perfect-scrollbar'
 
 // funcName() is used for error message customization
 export const checkStringArgValid = (elem: any, funcName: any): any => {
-  if (
-    !elem ||
-    typeof elem !== 'string' ||
-    elem.trim() === ''
-  ) {
-    throw new Error(`Provide a valid non-empty string argument to ${funcName} function`)
+  if (!elem || typeof elem !== "string" || elem.trim() === "") {
+    throw new Error(
+      `Provide a valid non-empty string argument to ${funcName} function`
+    );
   }
 
-  return true
-}
+  return true;
+};
 
 export const checkElemArgValid = (elem: any, funcName: any): any => {
-  if (
-    !elem ||
-    !(elem instanceof window.Element)
-  ) {
-    throw new Error(`Provide a valid DOM-element argument to ${funcName} function`)
+  if (!elem || !(elem instanceof window.Element)) {
+    throw new Error(
+      `Provide a valid DOM-element argument to ${funcName} function`
+    );
   }
 
-  return true
-}
+  return true;
+};
 
 // *** STRINGS RELATED
 export const checkTextEmpty = (stringArg: string) => {
-  return stringArg.length === 0 || stringArg.trim() === ''
-}
+  return stringArg.length === 0 || stringArg.trim() === "";
+};
 
 export const stringToCamelCase = (string: any, separator: any): any => {
-  checkStringArgValid(string, 'stringToCamelCase')
-  checkStringArgValid(separator, 'stringToCamelCase')
+  checkStringArgValid(string, "stringToCamelCase");
+  checkStringArgValid(separator, "stringToCamelCase");
 
-  const strArr = string.split(separator)
+  const strArr = string.split(separator);
 
   const newStrArr = strArr.map((elem: any, index: any) => {
     if (index === 0) {
-      return elem
+      return elem;
     }
 
-    const newElem = elem.slice(0, 1).toUpperCase() + elem.slice(1, elem.length)
-    return newElem
-  })
+    const newElem = elem.slice(0, 1).toUpperCase() + elem.slice(1, elem.length);
+    return newElem;
+  });
 
-  return newStrArr.join('')
-}
+  return newStrArr.join("");
+};
 
 // *** UI-RELATED
 
 export const hideElem = (elem: any): any => {
-  checkElemArgValid(elem, 'hideElem')
+  checkElemArgValid(elem, "hideElem");
 
-  elem.classList.add('js-hidden')
-}
+  elem.classList.add("js-hidden");
+};
 
 export const showElem = (elem: any): any => {
-  checkElemArgValid(elem, 'showElem')
+  checkElemArgValid(elem, "showElem");
 
-  elem.classList.remove('js-hidden')
-}
+  elem.classList.remove("js-hidden");
+};
 
 export const markAsError = (elem: any): any => {
-  checkElemArgValid(elem, 'markAsError')
+  checkElemArgValid(elem, "markAsError");
 
-  elem.classList.add('is-invalid')
-}
+  elem.classList.add("is-invalid");
+};
 
 // use if you need to remove error markers from element (check previous function)
 export const unmarkErrored = (elem: any): any => {
-  checkElemArgValid(elem, 'unmarkErrored')
+  checkElemArgValid(elem, "unmarkErrored");
 
-  elem.classList.remove('is-invalid')
-}
+  elem.classList.remove("is-invalid");
+};
 
 // Bootstrap related function for visual button state toggling
 export const toggleBootstrapBtn = (btn: any): any => {
-  checkElemArgValid(btn, 'toggleBootstrapBtn')
-  btn.classList.toggle('active')
-}
+  checkElemArgValid(btn, "toggleBootstrapBtn");
+  btn.classList.toggle("active");
+};
 
 export const toAutofocusElem = (elem: any): any => {
-  checkElemArgValid(elem, 'toAutofocusElem')
+  checkElemArgValid(elem, "toAutofocusElem");
 
-  elem.focus()
-}
+  elem.focus();
+};
 
 // *** TASKS RELATED
 
 // hides all task elements except tasks from toRenderArr
-export const renderTasksFromArrayOnly = (fullArr: any, toRenderArr: any): any => {
+export const renderTasksFromArrayOnly = (
+  fullArr: any,
+  toRenderArr: any
+): any => {
   if (
     !(fullArr && toRenderArr) ||
     !(Array.isArray(fullArr) && Array.isArray(toRenderArr))
   ) {
-    throw new Error('Please, provide a valid array in the renderTasksFromArrayOnly function')
+    throw new Error(
+      "Please, provide a valid array in the renderTasksFromArrayOnly function"
+    );
   }
 
-  fullArr.forEach(elem => {
-    const elemID = elem.taskId
-    const elemInUI = document.querySelector(`[data-task-id='${elemID}']`)
+  fullArr.forEach((elem) => {
+    const elemID = elem.taskId;
+    const elemInUI = document.querySelector(`[data-task-id='${elemID}']`);
 
-    let toShowElem = false
+    let toShowElem = false;
 
     for (let i = 0; i < toRenderArr.length; i++) {
       if (toRenderArr[i].taskId === elemID) {
-        toShowElem = true
-        break
+        toShowElem = true;
+        break;
       }
     }
 
     if (toShowElem) {
-      showElem(elemInUI)
+      showElem(elemInUI);
     } else {
-      hideElem(elemInUI)
+      hideElem(elemInUI);
     }
-  })
-}
+  });
+};
 
 // *** TEMPLATES RELATED
 
 export const grabTemplate = (templateElem: any): any => {
-  checkElemArgValid(templateElem, 'grabTemplate')
+  checkElemArgValid(templateElem, "grabTemplate");
 
-  return document.importNode(templateElem.content, true)
-}
+  return document.importNode(templateElem.content, true);
+};
 
-export const appendTemplate = (templateElem: any, appendDestinationElem: any): any => {
-  if (
-    !templateElem ||
-    !(templateElem instanceof window.DocumentFragment)
-  ) {
-    throw new Error('Add a valid template element to appendTemplate function')
+export const appendTemplate = (
+  templateElem: any,
+  appendDestinationElem: any
+): any => {
+  if (!templateElem || !(templateElem instanceof window.DocumentFragment)) {
+    throw new Error("Add a valid template element to appendTemplate function");
   }
 
-  checkElemArgValid(appendDestinationElem, 'appendTemplate')
+  checkElemArgValid(appendDestinationElem, "appendTemplate");
 
-  appendDestinationElem.append(templateElem)
-}
+  appendDestinationElem.append(templateElem);
+};
 
 // *** SCROLLBAR RELATED
 export const scrollbarIniting = (elemWithScrollbar: any): any => {
-  checkStringArgValid(elemWithScrollbar, 'scrollbarIniting')
+  checkStringArgValid(elemWithScrollbar, "scrollbarIniting");
 
-  const ps = new PerfectScrollbar(elemWithScrollbar)
-}
+  const ps = new PerfectScrollbar(elemWithScrollbar);
+};
