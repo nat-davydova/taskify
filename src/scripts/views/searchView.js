73d2d4c337cb2096a21a9e286888c775c7282d9a
nat-davydova <natti.davydova@gmail.com>
2021-01-31 15:55:31 +0300

    refactor(search view): port to TS

--- src/scripts/views/searchView.js
+++
@@ -1,5 +0,0 @@
-import { renderTasksFromArrayOnly } from "../utils";
-
-export const renderSearchResults = (fullList, searchedTasksArr) => {
-  renderTasksFromArrayOnly(fullList, searchedTasksArr);
-};
