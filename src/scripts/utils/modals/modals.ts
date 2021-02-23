458a66e73fe32879bd48a41e53a35b85d2d27555
nat-davydova <natti.davydova@gmail.com>
2021-02-23 19:09:31 +0300

    refactor(modal utils): refactor closeModal()

--- src/scripts/utils/modals/modals.ts
+++ src/scripts/utils/modals/modals.ts
@@ -1,25 +1,37 @@
 import { PATH } from "../../configs";
 import { unmarkErrored, setFocusToElem } from "../utils";
 
-export const closeModal = (modal: HTMLElement) => {
-  const modalInputs = modal.querySelectorAll("input");
-  modalInputs.forEach((elem) => {
-    unmarkErrored(elem);
-    // eslint-disable-next-line no-param-reassign
-    elem.value = "";
-  });
+const UI_EFFECTS_TIMEOUT_MS = 250;
 
+function cleanInputvalue(input: HTMLInputElement): void {
+  const inputElem = input;
+  inputElem.value = "";
+}
+
+function removeBackdrop(): void {
   const modalBackdrop = document.querySelector(
     PATH.modals.modalBackdrop
   ) as HTMLElement;
+
   const appFrame = document.querySelector(PATH.layout.frame) as HTMLElement;
 
   setTimeout(() => {
     appFrame.removeChild(modalBackdrop);
-  }, 250);
+  }, UI_EFFECTS_TIMEOUT_MS);
+}
+
+export function closeModal(modal: HTMLElement): void {
+  const modalInputs = modal.querySelectorAll("input");
+
+  modalInputs.forEach((elem) => {
+    unmarkErrored(elem);
+    cleanInputvalue(elem);
+  });
 
   modal.classList.remove("show");
-};
+
+  removeBackdrop();
+}
 
 export const openModal = (modal: string, elemToAutofocus?: string) => {
   const modalEl = document.querySelector(modal) as HTMLElement;
